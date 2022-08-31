7130b26d92ecb1faeab28e01975f98faaa141e6d
Mohiman Bhuiya <61213934+mohimanb@users.noreply.github.com>
2022-08-31 08:20:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,17 +14,13 @@ function App(){
     }        
   ])
 //add items
-  const [value, setValue] = React.useState('');
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (!value) return;
-    const newTodos = [...todos, {text:value, isCompleted:false}];
-    setTodos(newTodos);
-    setValue('');
-  }
+const addTodo = text => {
+  const newTodos = [...todos, {text:value, isCompleted:false}];
+  setTodos(newTodos);
+}
+
 //remove todo items
-  const removeTodo = e => {
-    const index = Number(e.target.id);
+  const removeTodo = index => {
     let temp = [...todos];
     temp.splice(index,1);
     setTodos(temp);
@@ -32,17 +28,8 @@ function App(){
   return(
     <>
       {todos.map((todo, i) => 
-        <div className="todo" key={i} id={i} onClick={removeTodo}>
-          {todo.text}</div>)}
-        <form onSubmit={handleSubmit}>
-          <input 
-                type="text"
-                claseName="input"
-                value={value}
-                placeholder="Add Task..."
-                onChange={e => setValue(e.target.value)}
-                />
-        </form>
+        <Todo index={i} key={i} todo={todo} remove={removeTodo} />)}
+      <TodoForm addTodo={addTodo}/>
     </>
   );
 }
