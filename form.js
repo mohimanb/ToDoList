91af59601146b41e35b5162f0c911b5bcddcd21d
Mohiman Bhuiya <61213934+mohimanb@users.noreply.github.com>
2022-08-31 08:32:48 -0400

    Update form.js

--- form.js
+++ form.js
@@ -1,25 +1,22 @@
-function TodoForm({addTodo}) {
-    const [value, setValue] = React.useState('');
+// user input - includes validation
+function TodoForm({addTodo}){
+  const [value,setValue] = React.useState('');
+  
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (!value) return;
-        addTodo(value);
-        setValue('');
-      }
-
-    return (
-        <>
-        <form onSubmit={handleSubmit}>
-        <input 
-              type="text"
-              claseName="input"
-              value={value}
-              placeholder="Add Task..."
-              onChange={e => setValue(e.target.value)}
-              />
-      </form>
-      </>
-     ) ;
-
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add Todo..."
+        onChange={e => setValue(e.target.value)} />
+    </form>
+  )
 }
